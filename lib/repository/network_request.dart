ed0d3fbbea1c0f3d484cf0001d1f38f8e0b47840
Maxvyr <maxvid30@gmail.com>
2022-04-11 22:05:20 +0200

    :heavy_plus_sign: dio + :heavy_minus_sign: http

--- lib/repository/network_request.dart
+++ lib/repository/network_request.dart
@@ -1,23 +1,35 @@
-import 'dart:convert';
-
+import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
-import 'package:http/http.dart' as http;
 import '../models/photo.dart';
 
-List<Photo> parsePhotos(String resBody) {
-  final list = json.decode(resBody) as List<dynamic>;
-  List<Photo> photos = list.map((element) => Photo.fromJson(element)).toList();
+List<Photo> parsePhotos(List<dynamic> resBody) {
+  // final list = json.decode(resBody) as List<dynamic>;
+  List<Photo> photos = resBody
+      .map(
+        (element) => Photo.fromJson(element),
+      )
+      .toList();
   return photos;
 }
 
 Future<List<Photo>> fetchPhotos() async {
-  final res = await http.get(
+  final res = await Dio().getUri(
     Uri.parse(
       "https://jsonplaceholder.typicode.com/photos",
     ),
   );
   if (res.statusCode == 200) {
-    return compute(parsePhotos, res.body);
+    final test = res.data;
+    Photo photo = Photo(
+      albumId: 12,
+      id: 12,
+      title: "title",
+      url: "url",
+      thumbnailUrl:
+          "https://konachan.com/sample/83278c0e183fd19232290e3ee443004b/Konachan.com%20-%20340361%20sample.jpg",
+    );
+    // return [photo];
+    return compute(parsePhotos, res.data as List<dynamic>);
   } else {
     throw Exception("Can't get photos");
   }
